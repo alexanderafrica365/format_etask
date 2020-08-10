c2c1e3051710e105b1f4173d80e7ce9fcb905c23
Martin Drlík <martin.drlik@email.cz>
2020-08-10 22:13:59 +0200

    update version.php (release 2.3 for Moodle [37, 39])

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020080900;
-$plugin->requires  = 2019111200;
-$plugin->supported = [38, 39];
+$plugin->version   = 2020081000;
+$plugin->requires  = 2019051100;
+$plugin->supported = [37, 39];
 $plugin->component = 'format_etask';
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '2.4';
+$plugin->release = '2.3';
