ab5b274c96f28b17379d74adbbf63c6c4f0f9c33
Martin Drlík <martin.drlik@email.cz>
2020-08-16 12:10:44 +0200

    fix versioning

--- lang/cs/format_etask.php
+++ lang/cs/format_etask.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,59 +16,29 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component eTask topics course format.
+ * Strings for component 'format_topics', language 'cs', branch 'MOODLE_27_STABLE'
  *
- * @package   format_etask
- * @copyright 2020, Martin Drlik <martin.drlik@email.cz>
+ * @package   format_topics
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Topics format strings.
-$string['addsections'] = 'Přidat témata';
+defined('MOODLE_INTERNAL') || die();
+
 $string['currentsection'] = 'Aktuální téma';
-$string['editsection'] = 'Upravit téma';
-$string['editsectionname'] = 'Upravit název tématu';
-$string['deletesection'] = 'Odstranit téma';
-$string['newsectionname'] = 'Nový název tématu {$a}';
-$string['sectionname'] = 'Téma';
-$string['pluginname'] = 'eTask tematické uspořádání';
-$string['section0name'] = 'Úvod';
-$string['page-course-view-topics'] = 'Hlavní stránka libovolného kurzu v eTask tématickém formátu';
-$string['page-course-view-topics-x'] = 'Jakákoliv stránka kurzu s eTask tematickým uspořádáním';
 $string['hidefromothers'] = 'Skrýt téma';
+$string['page-course-view-topics'] = 'Hlavní stránka libovolného kurzu v eTask formátu';
+$string['page-course-view-topics-x'] = 'Jakákoliv stránka kurzu s eTask uspořádáním';
+$string['pluginname'] = 'eTask uspořádání';
+$string['section0name'] = 'Úvod';
+$string['sectionname'] = 'Téma';
 $string['showfromothers'] = 'Zobrazit téma';
-$string['privacy:metadata'] = 'Modul Formát eTask tématické uspořádání neukládá žádné osobní údaje.';
-// Course format settings strings.
-$string['privateview'] = 'eTask soukromé zobrazení';
-$string['privateview_help'] = 'Toto nastavení určuje, zda mohou všichni studenti vidět své známky navzájem v tabulce hodnocení eTask nebo ne.';
-$string['privateview_no'] = 'Každý student může vidět známky ostatních studentů kurzu/skupiny';
-$string['privateview_yes'] = 'Přihlášený student může vidět pouze své vlastní známky';
-$string['progressbars'] = 'eTask progress bary';
-$string['progressbars_help'] = 'Toto nastavení určuje, zda se počítají progress bary Dokončeno a Splněno v nápovědě aktivity tabulky hodnocení eTask nebo ne.';
-$string['progressbars_donotcalculate'] = 'Nepočítat progress bary Dokončeno a Splněno';
-$string['progressbars_calculate'] = 'Počítat progress bary Dokončeno a Splněno';
-$string['studentsperpage'] = 'eTask počet studentů na stránce';
-$string['studentsperpage_help'] = 'Toto nastavení určuje počet studentů na stránce v tabulce hodnocení eTask.';
-$string['activitiessorting'] = 'eTask řazení aktivit';
-$string['activitiessorting_help'] = 'Toto nastavení určuje, zda jsou aktivity hodnotící tabulky eTask seřazeny od nejnovějších, od nejstarších nebo tak, jak jsou v kurzu.';
-$string['activitiessorting_latest'] = 'Řadit aktivity od nejnovějších';
-$string['activitiessorting_oldest'] = 'Řadit aktivity od nejstarších';
-$string['activitiessorting_inherit'] = 'Řadit aktivity tak, jak jsou v kurzu';
-$string['placement'] = 'eTask umístění';
-$string['placement_help'] = 'Toto nastavení určuje umístění tabulky hodnocení eTask nad nebo pod tématy kurzu.';
-$string['placement_above'] = 'Umístit hodnotící tabulku nad témata kurzu';
-$string['placement_below'] = 'Umístit hodnotící tabulku pod témata kurzu';
-// Plugin settings strings.
-$string['registeredduedatemodules'] = 'Registrované moduly s datumem odevzdání';
-$string['registeredduedatemodules_help'] = 'Určuje, v jakém databázovém poli modulu je ukládána hodnota datuu odevzání.';
-// The eTask grading table strings.
-$string['gradesavingsuccess'] = 'Potřebná známka pro <strong>{$a->itemName}</strong> byla úspěšně nastavena na hodnotu <strong>{$a->gradePass}</strong>.';
-$string['gradesavingerror'] = 'Chyba při ukládání potřebné známky pro <strong>{$a}</strong>. Prosím, zkuste to znovu později nebo kontaktujte vývojáře pluginu.';
+
+$string['nodata'] = 'Pro modul eTask nejsou k dispozici žádná data.';
+$string['gradesettings'] = 'Nastavení známek';
 $string['legend'] = 'Legenda';
-$string['activitycompleted'] = 'Dokončeno';
-$string['activitypassed'] = 'Splněno';
-$string['activityfailed'] = 'Nesplněno';
-$string['etask:teacher'] = 'Plná správa';
-$string['etask:noneditingteacher'] = 'Jen pro čtení (všechna data)';
-$string['etask:student'] = 'Jen pro čtení (data v kontextu soukromého zobrazení)';
-$string['notset'] = 'nenastaveno';
+$string['submitted'] = 'Odevzdáno';
+$string['passed'] = 'Splněno';
+$string['failed'] = 'Nesplněno';
+$string['notset'] = 'Nenastaveno';
+$string['save'] = 'Uložit';
