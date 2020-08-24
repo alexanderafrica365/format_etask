76d52ca5032e95b995cc1ae27cdd8e069e743658
Martin Drlík <martin.drlik@email.cz>
2020-08-24 20:30:37 +0200

    upgrade version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020081800;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020082400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->supported = [33, 33];
 $plugin->incompatible = 34;
