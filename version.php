8115b9f447cfada51d89a77241e231e0d073eaaa
Martin Drlík <martin.drlik@email.cz>
2020-08-27 18:44:27 +0200

    fix plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020082400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020082701;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012112900;        // Requires this Moodle version.
 $plugin->supported = [24, 24];
 $plugin->incompatible = 25;
